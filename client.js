de2a4bede0f0b466829bb9c62228a97a802b8798
Li-Heng Liang <admin@lihengl.com>
2016-04-08 04:06:01 +0800

    Format sensor readings into a rotating array

--- client.js
+++ client.js
@@ -1,7 +1,31 @@
-io.connect('http://localhost:3000').on('sense', function (data) {
-  console.warn(data);
+var readings = _.fill(Array(180), {
+  accelerometer: {x: 0, y: 0, z: 0},
+  gyroscope: {x: 0, y: 0, z: 0},
+  gravity: {x: 0, y: 0, z: 0},
+  rotation: {s: 0, x: 0, y: 0, z: 0}
 });
 
-window.setInterval(function () {
-  console.clear();
-}, 3000);
+
+var parseReading = function (messageSegments) {
+  var value = {}, values = [];
+  try { values = JSON.parse(messageSegments[1]); }
+  catch (e) { return null; }
+  value = {x: values[0], y: values[1], z: values[2]};
+  switch (messageSegments[0]) {
+    case 'ACL': return {accelerometer: value};
+    case 'GRS': return {gyroscope: value};
+    case 'GVT': return {gravity: value};
+    case 'ROT': return {rotation: _.assign(value, {s: values[3]})};
+    default:
+      console.error('Unexpected message segments: ', messageSegments);
+      return null;
+  }
+};
+
+
+io.connect('http://localhost:3000').on('update', function (message) {
+  var reading = parseReading(message.split(': '));
+  if (_.isNull(reading)) { return; }
+  readings.shift();
+  readings.push(_.defaults(reading, _.last(readings)));
+});
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var http = require('http');
 var socket = require('socket.io');
+var _ = require('lodash');
+
 
 var server = http.Server(express());
 var io = socket(server);
@@ -13,9 +15,13 @@ server.listen(3000);
 
 
 BrowserStream._write = function (chunk, enc, next) {
-  io.sockets.emit('sense', chunk.toString('utf8'));
+  var logMessage = chunk.toString('utf8');
+  var sensorKeyword = 'SENSOR-';
+  if (!_.includes(logMessage, sensorKeyword)) { return next(); }
+  io.sockets.emit('update', logMessage.split(sensorKeyword)[1]);
   next();
 };
+
 
 process.stdin.pipe(BrowserStream);
 process.stdin.pipe(process.stdout);
